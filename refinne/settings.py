6c3f36faaf9bc38e3442082c12d02700e50159a5
Aryann Dwivedi <aryanndwivedi2403@gmail.com>
2022-12-26 21:18:59 +0530

    4

--- refinne/settings.py
+++ refinne/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-5cff.up.railway.app/']
+
+
