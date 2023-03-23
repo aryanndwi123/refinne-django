e82d0c66d6b590d3e8b5fc064fb6e4d60eb10884
Aryann Dwivedi <aryanndwivedi2403@gmail.com>
2023-03-24 00:25:03 +0530

    feat(backend):changes in db1

--- refinne/settings.py
+++ refinne/settings.py
@@ -88,11 +88,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.postgresql',
         'ENGINE': 'djongo',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'refinnedb',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'kJ6Wc11MmnWXBgIeXvj4',
-        # 'HOST': 'containers-us-west-164.railway.app',
-        # 'PORT': '5647',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '1D0bozHQ0WFhviswR0sq',
+        'HOST': 'containers-us-west-92.railway.app',
+        'PORT': '6169',
     }
 }
 
