2f1f8f483f9ced15997e2f94526e9f6961283f2e
Aryann Dwivedi <aryanndwivedi2403@gmail.com>
2023-06-10 00:06:12 +0530

    feat(frontend): changing cards of coupon

--- blogs/views.py
+++ blogs/views.py
@@ -1,15 +1,3 @@
-from django.shortcuts import render, HttpResponse,redirect
-from coupons.models import Coupons
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate , login ,logout
-from django.contrib import messages
-import random
-from django.db import models
-from django.utils import timezone
+from django.shortcuts import render
 
-
-
-def blogHome(request):
-    return render('home/home.html')
-
-
+# Create your views here.
